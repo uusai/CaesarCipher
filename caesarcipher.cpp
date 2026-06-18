a4162ac2ba5179542ab54ccfdaad3f4fcf1b5a8d
uusai <dimonics228@gmail.com>
2026-06-19 01:34:16 +0800

    Первый коммит

---
+++ caesarcipher.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class CaesarCipher{
+    int Value;
+    string Text;
+public:
+    CaesarCipher(string text, int value){
+        Value=value;
+        Text=text;
+    }
+    string Encrypt(){
+
+    }
+    string Decipher(){
+
+    }
+};
+
+int main(){
+
+}
