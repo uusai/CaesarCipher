5e5ab0d91a6ae3c8e2d07fa3fed00bca8233ac31
uusai <dimonics228@gmail.com>
2026-06-22 01:11:37 +0800

    modify AutoEncrypt

--- caesarcipher.cpp
+++ caesarcipher.cpp
@@ -2,27 +2,34 @@
 #include <string>
 #include <vector>
 #include <cctype>
+#include <algorithm>
 
 using namespace std;
 
 struct LetterAndValue{
     int Value;
     char Letter;
-    LetterAndValue operator++(int){
+    LetterAndValue& operator++(int){
+        LetterAndValue temp = *this;
         Value++;
+        return *this;
     }
     LetterAndValue(){
         Value = 0;
         Letter = '-';
     }
-    char Set_Letter(char Letter){
+    bool Set_Letter(char Letter){
         this->Letter = Letter;
+        return true;
     }
     int Get_Value(){
         return Value;
     }
     char Get_Letter(){
         return Letter;
+    }
+    void Print(){
+        cout << Letter << " val: " << Value;
     }
 };
 
@@ -92,28 +99,43 @@ public:
 };
 
 class AutoEncrypt{
+    vector<LetterAndValue> value = {};
 public:
     AutoEncrypt(string text){
-        vector<LetterAndValue> value = {};
         for(int i = 0; i < text.size(); i++){
+            if(text[i] == ' ' || text[i] == '.' || text[i] == ',') continue;
+            bool Check = false;
             for(int j = 0; j < value.size(); j++){
-                if(value[j].Get_Letter() == text[j]){
+                if(value[j].Get_Letter() == text[i]){
                     value[j]++;
+                    Check = true;
                     break;
                 }
-                else{ //Переделать так, чтобы была проверка if прошел массив и не нашел нужную букву
+            }
+            if(Check == false){
                     value.emplace_back();
                     value[value.size()-1]++;
                     value[value.size()-1].Set_Letter(text[i]);
-                }
             }
+        }
+        sort(value.begin(), value.end(), [](const LetterAndValue& a, const LetterAndValue& b) {
+            return a.Value > b.Value; 
+        });
+    }
+    void Print(){
+        for(int i = 0; i < value.size(); i++){
+            value[i].Print();
+            cout << '\n';
         }
     }
 };
 
 int main(){
-    cout << CaesarCipher::Encrypt("Hello World! 3232", 3);
-    
+    //cout << CaesarCipher::Encrypt("Hello World! 3232", 3);
+    string text = "Hi. My names John Miller, Im from Francisco.";
+    //cout << text.size();
+    AutoEncrypt A(text);
+    A.Print();
     // while(true){
     //     string Command;
     //     cin >> Command;
